55a2dcd4b870024e1eeb94060f32fef7a2c6e502
kkuyu <devils67@naver.com>
2021-05-09 21:39:58 +0900

    스토어 속성 모듈화

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
-import { fetchAskList, fetchJobsList, fetchNewsList } from "../api/index";
+import mutations from "./mutations";
+import actions from "./actions";
 
 Vue.use(Vuex);
 
@@ -16,35 +17,6 @@ export const store = new Vuex.Store({
 			return state.ask;
 		},
 	},
-	mutations: {
-		SET_NEWS(state, news) {
-			state.news = news;
-		},
-		SET_JOBS(state, jobs) {
-			state.jobs = jobs;
-		},
-		SET_ASK(state, ask) {
-			state.ask = ask;
-		},
-	},
-	actions: {
-		FETCH_NEWS(context) {
-			fetchNewsList()
-				.then((response) => {
-					console.log(response.data);
-					context.commit("SET_NEWS", response.data);
-				})
-				.catch((error) => console.log(error));
-		},
-		FETCH_JOBS({ commit }) {
-			fetchJobsList()
-				.then(({ data }) => commit("SET_JOBS", data))
-				.catch((error) => console.log(error));
-		},
-		FETCH_ASK({ commit }) {
-			fetchAskList()
-				.then(({ data }) => commit("SET_ASK", data))
-				.catch((error) => console.log(error));
-		},
-	},
+	mutations,
+	actions,
 });
