5e8659e3aed70ad61f6a429e2548510ae4d9f5bc
kkuyu <devils67@naver.com>
2021-05-09 20:04:44 +0900

    axios를 이용한 api 호출

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,9 +1,32 @@
 <template>
-	<div>news</div>
+	<div>
+		news
+		<div v-for="news in newsList">{{ news }}</div>
+	</div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+	data() {
+		return {
+			newsList: [],
+		};
+	},
+	created() {
+		var vm = this;
+		axios
+			.get("https://api.hnpwa.com/v0/news/1.json")
+			.then(function(response) {
+				console.log(response);
+				vm.newsList = response.data;
+			})
+			.catch(function(error) {
+				console.log(error);
+			});
+	},
+};
 </script>
 
 <style></style>
