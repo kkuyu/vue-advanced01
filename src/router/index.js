3734cb015ebdeb74fb59d3423b9ae6ce2ddd0af9
kkuyu <devils67@naver.com>
2021-05-09 19:31:57 +0900

    router-view를 이용한 라우팅 컴포넌트 표시

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+import NewsView from "../views/NewsView";
+import JobsView from "../views/JobsView";
+import AskView from "../views/AskView";
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+	routes: [
+		{ path: "/news", component: NewsView },
+		{ path: "/jobs", component: JobsView },
+		{ path: "/ask", component: AskView },
+	],
+});
---
+++ src/views/JobsView.vue
@@ -0,0 +1,9 @@
+<template>
+	<div>job</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
