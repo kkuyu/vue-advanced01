50048a606f21004e4bda881b29dad53c71224c18
kkuyu <devils67@naver.com>
2021-05-10 19:45:18 +0900

    라우터 트랜지션

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
 	<div id="app">
 		<tool-bar />
-		<router-view></router-view>
+		<transition name="page">
+			<router-view></router-view>
+		</transition>
 	</div>
 </template>
 
@@ -17,5 +19,12 @@ export default {
 body {
 	margin: 0;
 	padding: 0;
+}
+
+.page-enter-active, .page-leave-active {
+	transition: opacity 0.5s;
+}
+.page-enter, .page-leave-to {
+	opacity: 0;
 }
 </style>
