c9e811ce54208986942a76610f5ff9cdc6f563ce
kkuyu <devils67@naver.com>
2021-05-12 19:12:16 +0900

    사용자 프로필 컴포넌트 소개 및 등록

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,5 +1,6 @@
 <template>
 	<div>
+    <UserProfile></UserProfile>
 		<p>name: {{ userInfo.id }}</p>
 		<p>karma: {{ userInfo.karma }}</p>
 		<p>created: {{ userInfo.created }}</p>
@@ -7,7 +8,12 @@
 </template>
 
 <script>
+import UserProfile from "../components/UserProfile.vue"
+
 export default {
+  components: {
+    UserProfile,
+  },
 	computed: {
 		userInfo() {
 			return this.$store.state.user;
